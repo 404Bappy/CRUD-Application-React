5e6823326e201d1d5da79591e24552f95383f50a
Bappy71 <19103076@iubat.edu>
2023-07-01 14:38:05 +0600

    first commit

--- Server/index.js
+++ Server/index.js
@@ -1,23 +1,23 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+// import express from "express";
+// import mongoose from "mongoose";
+// import cors from "cors";
 
-import posts from "./routes/posts.js";
+// import posts from "./routes/posts.js";
 
-const app = express();
-app.use(cors());
+// const app = express();
+// app.use(cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/posts", posts);
+// app.use("/api/posts", posts);
 
-mongoose
-  .set({ strictQuery: false })
-  .connect("mongodb://127.0.0.1/blog")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error(`Could not connected to MongoDB... ${err}`));
+// mongoose
+//   .set({ strictQuery: false })
+//   .connect("mongodb://127.0.0.1/blog")
+//   .then(() => console.log("Connected to MongoDB..."))
+//   .catch((err) => console.error(`Could not connected to MongoDB... ${err}`));
 
-const port = process.env.PORT || 9000;
+// const port = process.env.PORT || 9000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// app.listen(port, () => console.log(`Listening on port ${port}`));
