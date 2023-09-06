c0bab9b9000a285fbc6a69fb242d576da6f82df7
Bappy71 <19103076@iubat.edu>
2023-09-06 10:29:38 +0600

    first commit

--- client/src/Pages/Posts/Posts.js
+++ client/src/Pages/Posts/Posts.js
@@ -1,3 +1,4 @@
+import config from "../../config.json";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./Posts.css";
@@ -5,9 +6,10 @@ import "./Posts.css";
 function Posts() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
-      const fatchPosts = async()=>{
-          const res = await axios.get()
-      }
+    const fatchPosts = async () => {
+      const { data } = await axios.get(config.apiUrl);
+      setPosts(data);
+    };
   }, []);
   return (
     <div className="posts">
